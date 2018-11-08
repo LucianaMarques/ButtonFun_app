9c6c8e3693b311a87d7acc6549de6663f7579f7a
Luciana <lmarques@MacBook-Pro-de-Jonatas.local>
2018-11-08 14:34:07 -0200

    added label and two buttons

--- ButtonFun/ViewController.swift
+++ ButtonFun/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var statusLabel: UILabel!
     @IBAction func buttonPressed(_ sender: UIButton) {
+        
     }
     
 }
